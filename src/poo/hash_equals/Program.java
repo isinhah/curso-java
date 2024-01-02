36ed0cb45947b10f7801a162c48c74471a569cec
isinhah <6isinha6@gmail.com>
2024-01-02 10:36:25 -0300

    add exercicios de hashCode e equals

---
+++ src/poo/hash_equals/Program.java
@@ -0,0 +1,12 @@
+package src.poo.hash_equals;
+
+public class Program {
+    public static void main(String[] args) {
+        Client c1 = new Client("maria", "maria@gmail.com");
+        Client c2 = new Client("alex", "maria@gmail.com");
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c1.equals(c2));
+    }
+}
